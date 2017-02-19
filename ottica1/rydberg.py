8f68139c3d4678519d6c406b73c5785206cdedab
gianfranco95 <gianfrancocordella@gmail.com>
2017-02-19 15:53:07 +0100

    fit rydberg torna abbastanza bene

--- ottica1/rydberg.py
+++ ottica1/rydberg.py
@@ -1,6 +1,6 @@
 import  os
 folder = os.path.realpath('.')
-from lab import *
+# from lab import *
 import numpy
 import math
 import pylab
@@ -12,7 +12,7 @@ rawdata = numpy.loadtxt(os.path.join(folder, 'Dati', datafile)).T
 theta=rawdata[0]+(rawdata[1]/60)
 
 theta0=168.71944444
-dtheta0=0.03333333
+dtheta0=numpy.sqrt(0.03333333**2 + (1/60)**2)
 media=numpy.empty(len(theta)/3)
 disp=numpy.empty(len(theta)/3)
 
@@ -25,7 +25,7 @@ while i <len(theta):
     i=i+3
 
 theta=media-(theta0*numpy.ones(2))
-dtheta=numpy.sqrt(disp**2 +(dtheta0*numpy.ones(2))**2)
+dtheta=numpy.sqrt( (disp/2)**2 +(1/60)**2+(dtheta0*numpy.ones(2))**2)
 #Determinazione del passo reticolare
 #misure
 a0=theta[0]
@@ -52,28 +52,24 @@ datafile = 'idrogeno.txt'
 gradi,primi,n_2,ord=pylab.loadtxt(os.path.join(folder, 'Dati', datafile)).T
 
 alfa=gradi +(primi)/60
-media=numpy.empty(len(alfa)/3)
-disp=numpy.empty(len(alfa)/3)
+media1=numpy.empty(len(alfa)/3)
+disp1=numpy.empty(len(alfa)/3)
 ordine=numpy.empty(len(alfa)/3)
 n2=numpy.empty(len(alfa)/3)
-n1=numpy.ones(len(alfa)/3)
+n1=numpy.ones(len(alfa)/3)*2
+
+
 i=0
 while i <len(alfa):
-    a=(alfa[i]+alfa[i+1]+alfa[i+2])/3
-    media[(1/3)*i]=a
+    media1[(1/3)*i] = (alfa[i]+alfa[i+1]+alfa[i+2])/3
     ordine[(1/3)*i]=ord[i]
     n2[(1/3)*i]=n_2[i]
     alfa[i:i+3].sort() 
-    disp[(1/3)*i]=alfa[i+2]-alfa[i]
+    disp1[(1/3)*i]=alfa[i+2]-alfa[i]
     i=i+3
 
-alfa=media-(theta0*numpy.ones(len(media)))
-dalfa=numpy.sqrt(disp**2 +(dtheta0*numpy.ones(len(media)))**2)
-# p=pylab.zeros(len(ordine))
-# for i in range(0,len(ordine)):
-#     if(n2[i]==-1): p[i]=1
-#     else: p[i]=0
-
+alfa=media1-(theta0*numpy.ones(len(media1)))
+dalfa=numpy.sqrt( ((disp1/2)**2+(1/60)**2) +(dtheta0*numpy.ones(len(media1)))**2)
 
 alfa=alfa*pylab.pi/180
 dalfa=dalfa*math.pi/180
@@ -87,19 +83,11 @@ x= 1/(n1**2) - 1/(n2**2)
 y=1/lamb
 dy=dlamb/(lamb**2)
 
-pylab.figure(1)
-pylab.xlabel(' x')
-pylab.ylabel('y')
-pylab.title('Data')
-pylab.grid(color='gray')
-pylab.plot(x,y, 'o')
-pylab.errorbar(x,y,yerr=dy,linestyle='')
-
 def f(x,m,q):
     return m*x+q
     
 from scipy.optimize import curve_fit
-popt, pcov=curve_fit(f,x,y,pylab.array([400.,0.]),sigma=dy,absolute_sigma=True)
+popt, pcov=curve_fit(f,x,y,pylab.array([0.01,0.]),sigma=dy,absolute_sigma=True)
 m_fit,q_fit=popt
 dm_fit,dq_fit=pylab.sqrt(pcov.diagonal())
 mq_cov=pcov[0,1]
@@ -113,7 +101,7 @@ print('Chi quadro/ndof = %f/%f\nprobabilità associata = %f'%(chisquare,dof,pchi
 #Grafico
 pylab.figure(2)
 pylab.subplot(211)
-pylab.xlim(0.87,0.97)
+pylab.xlim(0.12,0.22)
 pylab.ylabel('1/$\lambda$ $[nm^{-1}]$', size = 15)
 pylab.title('misura costante di Rydberg')
 pylab.grid(color='gray')
@@ -124,7 +112,7 @@ pylab.subplot(212)
 pylab.title('Residui')
 pylab.xlabel('$1/n_1^{2}-1/n_2^{2}$', size = 15)
 pylab.plot(x,(y-f(x,m_fit,q_fit))/dy,'o',linestyle='',markersize = 5)
-pylab.xlim(0.87,0.97)
+pylab.xlim(0.12,0.22)
 pylab.legend(loc = 4)
 pylab.show()
 
@@ -134,18 +122,18 @@ rawdataa = numpy.loadtxt(os.path.join(folder, 'Dati', datafile)).T
 theta1=rawdataa[0]+(rawdataa[1]/60)
 theta0=168.71944444
 dtheta0=0.03333333
-media=numpy.empty(len(theta1)/3)
-disp=numpy.empty(len(theta1)/3)
+media2=numpy.empty(len(theta1)/3)
+disp2=numpy.empty(len(theta1)/3)
 
 i=0
 while i <len(theta1):
     a=(theta1[i]+theta1[i+1]+theta1[i+2])/3
-    media[(1/3)*i]=a
+    media2[(1/3)*i]=a
     theta1[i:i+3].sort() 
-    disp[(1/3)*i]=theta1[i+2]-theta1[i]
+    disp2[(1/3)*i]=theta1[i+2]-theta1[i]
     i=i+3
 
-theta1=(media-(theta0*numpy.ones(2)))*math.pi/180
+theta1=(media2-(theta0*numpy.ones(2)))*math.pi/180
 theta1=(pylab.pi-ti)*pylab.ones(2)-theta1
 dtheta1=numpy.sqrt(disp**2 +((dtheta0-(dti*180/math.pi))*numpy.ones(2))**2)*math.pi/180
 lambda_sodio=(d*(pylab.sin(ti)-pylab.sin(theta1)))/1

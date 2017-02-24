f0abebc4621a564f926c60d1271ae2f03cd4e180
gianfranco95 <gianfrancocordella@gmail.com>
2017-02-24 23:11:18 +0100

    ho completato la parte 1 con grafici e tabelle. resta da finire la parte2, i grafici necessari li ho messi nella cartella fig_tabs, dovrebbero bastare

--- Franck-Hertz/lettura_osc.py
+++ Franck-Hertz/lettura_osc.py
@@ -4,10 +4,27 @@ import pylab as py
 import numpy as np
 folder = os.path.realpath('.')
 
-filename='data3.csv'
+filename='data626.csv'
 o=oscillografo.OscilloscopeData(os.path.join(folder, 'Dati', filename))
+o.CH1=o.CH1*10
 
-# py.plot(o.T1,o.CH1)
-py.plot(o.T2,o.CH2)
+py.plot(o.CH1,o.CH2,'.')
+py.xlabel('$V_a$ [V]')
+py.ylabel('$ \propto I_c$')
+py.title('$I_c$ vs $V_a$ ')
+py.xlim(1,82)
 py.show()
 
+##max e min
+datafile='maxmin.txt'
+rawdata = np.loadtxt(os.path.join(folder, 'Dati', datafile)).T
+
+i=10
+
+py.figure(i)
+py.xlabel('$V_e$ [V]')
+py.ylabel('$V_a$ [V]')
+py.title('valori di $V_a$ vs $V_e$ per terzo minimo di $I_c$')
+py.errorbar(rawdata[12],rawdata[i],rawdata[i+1],rawdata[13],'o')
+i=i+2
+py.show()

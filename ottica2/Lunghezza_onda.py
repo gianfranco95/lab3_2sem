7bc5d92681bc3cacb92711616faa32673abcf717
gianfranco95 <gianfrancocordella@gmail.com>
2017-03-09 22:54:58 +0100

    inserita la media

--- ottica2/Lunghezza_onda.py
+++ ottica2/Lunghezza_onda.py
@@ -11,10 +11,22 @@ D = 45 #distanza tra schermo e laser su reticolo
 dD = 20
 
 datafile = 'lunghezza_onda.txt'
-rawdata = numpy.loadtxt(os.path.join(folder, datafile)).T
-h = rawdata[0]
-dh = rawdata[1]
-x = rawdata[2]
+rawdata = numpy.loadtxt(os.path.join(folder,'Dati', datafile)).T
+h = numpy.empty(len(rawdata[0])/3)
+dh =numpy.empty(len(rawdata[0])/3)
+x = numpy.empty(len(rawdata[0])/3)
+disp= numpy.empty(len(rawdata[0])/3) #dispersione tra i 3 valori misurati da confrontare con dh
+
+#media sulle tre misure fatte
+i=0
+while i <len(rawdata[0]):
+    a=(rawdata[0][i]+rawdata[0][i+1]+rawdata[0][i+2])/3
+    h[(1/3)*i]=a
+    rawdata[0][i:i+3].sort() 
+    disp[(1/3)*i]=rawdata[0][i+2]-rawdata[0][i]
+    dh[(1/3)*i]=(rawdata[1][i]+rawdata[1][i+1]+rawdata[1][i+2])/3
+    x[(1/3)*i]=rawdata[2][i]
+    i=i+3
 
 yr = 4 #posizione del raggio sullo schermo in assenza di reticolo
 dyr = 1
@@ -31,7 +43,8 @@ def f(x,m,q):
     
 from scipy.optimize import curve_fit
 popt, pcov=curve_fit(f,x,y,pylab.array([400.,0.]),sigma=dy,absolute_sigma=True)
-m0,q0=popt
+m0=popt[0]
+q0=popt[1]
 dm0,dq0=pylab.sqrt(pcov.diagonal())
 cov = pcov[0,1]/(dm0*dq0)
 print ('Pendenza fit = ',m0,dm0)
@@ -40,6 +53,22 @@ print ('Covarianza = ',cov)
 chisq = (((y-f(x,m0,q0))/dy)**2).sum()
 ndof = len(y)-2
 print ('Chi quadro/ dof = ',chisq, ndof)
+
+#Grafico
+pylab.figure(2)
+pylab.subplot(211)
+pylab.ylabel('y')
+pylab.title('Data')
+pylab.grid(color='gray')
+pylab.plot(x,y, 'o')
+pylab.errorbar(x,y,yerr=dy,xerr=None,linestyle='')
+pylab.plot(x,f(x,m0,q0), color='black',label = "retta di fit")
+pylab.subplot(212)
+pylab.title('Residui')
+pylab.xlabel(' m')
+pylab.plot(x,(y-f(x,m0,q0))/dy,'o',linestyle='',markersize = 5)
+pylab.legend(loc = 4)
+pylab.show()
 
 d = 0.1 #passo reticolare
 dd = 0.01

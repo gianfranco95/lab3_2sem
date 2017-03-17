7d3bb2beb91b3c9c69c689b10992c5f108056819
Luca <luca.ciambriello29@gmail.com>
2017-03-17 16:23:48 +0100

    Aggiustati

--- e_m/campo_magnetico.py
+++ e_m/campo_magnetico.py
@@ -10,11 +10,6 @@ from importare_dati import importa
 # N numero spire
 # r0 raggio bobina
 #punto da analizzare in posizione (y0,z0)
-datafile=['datiB6.txt']
-V,r=importa(datafile)
-pylab.figure(3)
-pylab.plot(r,V,'o')
-pylab.show()
 
 def rx(phi,r0):
     return -r0*pylab.cos(phi)
@@ -61,29 +56,29 @@ def Bz_tot(A,r0,y0,z0,s,d):
     return Bz(A,r0,y0,z0+d/2,s) + Bz(A,r0,y0,z0-d/2,s)
 def By_tot(A,r0,y0,z0,s,d):
     return By(A,r0,y0,z0+d/2,s) + By(A,r0,y0,z0-d/2,s)
-#I nostri casi
-A0=10**(-7)*130
-r00=0.15
-s0=1
-d0=r00
-z=pylab.linspace(-r00/2,r00/2,100)
-pylab.figure(1)
-pylab.title('Bz')
-pylab.xlabel('z')
-pylab.plot(z, Bz_tot(A0,r00,0,z,s0,d0)/Bz_tot(A0,r00,0,0,s0,d0))
-pylab.show()
-
-y=pylab.linspace(-r00,r00,100)
-pylab.figure(2)
-pylab.subplot(211)
-pylab.title('Bz')
-pylab.xlabel('y')
-pylab.plot(y, Bz_tot(A0,r00,y,0,s0,d0)/Bz_tot(A0,r00,0,0,s0,d0))
-pylab.subplot(212)
-pylab.title('By')
-pylab.xlabel('y')
-pylab.plot(y, 10**(100)*By_tot(A0,r00,y,-r00/2,s0,d0)/Bz_tot(A0,r00,0,0,s0,d0))
-pylab.show()
+# #I nostri casi
+# A0=10**(-7)*130
+# r00=0.15
+# s0=1
+# d0=r00
+# z=pylab.linspace(-r00/2,r00/2,100)
+# pylab.figure(1)
+# pylab.title('Bz')
+# pylab.xlabel('z')
+# pylab.plot(z, Bz_tot(A0,r00,0,z,s0,d0)/Bz_tot(A0,r00,0,0,s0,d0))
+# pylab.show()
+# 
+# y=pylab.linspace(-r00,r00,100)
+# pylab.figure(2)
+# pylab.subplot(211)
+# pylab.title('Bz')
+# pylab.xlabel('y')
+# pylab.plot(y, Bz_tot(A0,r00,y,0,s0,d0)/Bz_tot(A0,r00,0,0,s0,d0))
+# pylab.subplot(212)
+# pylab.title('By')
+# pylab.xlabel('y')
+# pylab.plot(y, 10**(100)*By_tot(A0,r00,y,-r00/2,s0,d0)/Bz_tot(A0,r00,0,0,s0,d0))
+# pylab.show()
 
 
         
--- e_m/setup.py
+++ e_m/setup.py
@@ -5,9 +5,18 @@ import math
 import pylab
 import scipy
 import scipy.special
+from campo_magnetico import Bz_tot
+from importare_dati import importa
 ##Calibrazione sonda
-Vout=15 #In volt
-dVout=1
+datafile=['datiB6.txt']
+V,d=importa(datafile)
+raggio=(31.6-1.9)/2
+r=raggio-(d-1.9)
+
+
+
+Vout=V/1000 #In volt
+dVout=1/1000
 #Fattore di amplificazione
 A=11.090
 dA=0.003
@@ -20,7 +29,36 @@ dVh=pylab.sqrt((dVout/A)**2 + (Vout*dA/(A**2))**2)
 
 B=Vh/Q #Gauss
 dB=pylab.sqrt((dVh/Q)**2 + (Vh*dQ/(Q**2))**2)
-print('Campo in Gauss = %f +- %f' % (B,dB))
+# print('Campo in Gauss = %f +- %f' % (B,dB))
+
+BT=B*10**(-4)
+rm=r*10**(-2)
+dBT=dB*10**(-4)
+
+pylab.figure(1)
+pylab.plot(rm,BT,'o')
+pylab.show()
 
 ##Altre cose che si possono fare: 
 # mappare Bz e fare fit a Bz/Bzmax per ottenere il raggio delle bobine che serve a corregger il campo magnetico per quando si farà il fit circolare per trovare e_m
+N=130
+def f(r,r0,A):
+    return Bz_tot(A,r0,r,0,1,r0)
+
+from scipy.optimize import curve_fit
+popt, pcov=curve_fit(f,rm,BT,pylab.array([0.15,0.]),sigma=dBT,absolute_sigma=True)
+r0=popt[0]
+A0=popt[1]
+dr0,dA0=pylab.sqrt(pcov.diagonal())
+cov = pcov[0,1]/(dA0*dr0)
+I0=(10**7)*A0/N
+dI0=(I0/A0)*dA0
+print ('Corrente in Ampere= ',I0,dI0)
+print ('raggio = ',r0,dr0)
+print ('Covarianza = ',cov)
+chisq = (((BT-f(rm,r0,A0))/dBT)**2).sum()
+ndof = len(B)-2
+print ('Chi quadro/ dof = ',chisq, ndof)
+x=pylab.linspace(min(rm),max(rm),1000)
+pylab.plot(x,f(x,r0,A0),'-')
+pylab.show()

26d7aa3122be26b130e8f618889e944137f3e985
Luca <luca.ciambriello29@gmail.com>
2017-03-19 18:23:23 +0100

    eerer

--- e_m/fit_luca.py
+++ e_m/fit_luca.py
@@ -214,7 +214,7 @@ def e_m(calib,i):
     pylab.show()
     
     ##calcolo e/m [valore atteso : 1.7588*10^(11) C/Kg]
-    g=1/g
+    # g=1/g
     R=g*r/alfa
     dR=pylab.sqrt((dg*r/alfa)**2 + (dr*g/alfa)**2+ (dalfa*R/alfa)**2)
     

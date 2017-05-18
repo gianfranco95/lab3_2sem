28b54b1f27f87440ab40c68900e1efb1c4aa6da1
beota <leonardouto@libero.it>
2017-05-18 19:34:34 +0200

    inizio

--- Lock-in/Lock.py
+++ Lock-in/Lock.py
@@ -1,24 +1,23 @@
 import pylab
 import  os
-#folder = os.path.realpath('.')
+folder = os.path.realpath('.')
 import numpy
 import math
 import pylab 
 import scipy
 import scipy.special
 
-#n,Vout=pylab.loadtxt(os.path.join(folder,'Dati','risposta_frequenza.txt')).T
-n,Vout=pylab.loadtxt('C:/Users/Leonardo/Desktop/Lock_in.txt',unpack=True)
+n,Vout=pylab.loadtxt(os.path.join(folder,'Dati','Lock_in.txt')).T
 dVout=pylab.sqrt((Vout*0.5/100)**2+1)
 from scipy.optimize import curve_fit
 
 def F(x,a,b):
-    return a*pylab.exp(-x/b)
+    return a*pylab.exp(-x*b)
 popt,pcov=curve_fit(F,n,Vout,p0=[1000,1],sigma=dVout)
 a0,b0=popt
 da0,db0=pylab.sqrt(pcov.diagonal())
 pylab.title('Tensione di uscita in funzione del numero di lastrine')
-pylab.xlabel('numero lastrine')
+pylab.subplot(211)
 pylab.ylabel('V$_{out}$ [mV]')
 pylab.errorbar(n,Vout,dVout,color = 'black',marker = '',linestyle='')
 r=pylab.linspace(-0.5,13,100)
@@ -26,3 +25,7 @@ pylab.plot(r,F(r,a0,b0),color = 'black')
 chisq=(((F(n,a0,b0)-Vout)/dVout)**2).sum()
 ndof=len(Vout)-2
 cov=pcov[0,1]/pylab.sqrt(pcov[0,0]*pcov[1,1])
+pylab.subplot(212)
+pylab.errorbar(n,((F(n,a0,b0)-Vout)/dVout),marker = 'o', color = 'black', linestyle ='')
+pylab.ylabel('Scarto normalizzato')
+pylab.xlabel('numero lastrine')
